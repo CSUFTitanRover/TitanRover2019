259de26245d09cb26545245e6572f5d86d3c2071
Can <Jayteare@users.noreply.github.com>
2018-08-05 02:35:04 -0700

    Updated the repository change.

--- build/scripts/ros-copy.sh
+++ build/scripts/ros-copy.sh
@@ -1,12 +1,11 @@
-git_repo_var="git@github.com:CSUFTitanRover/TitanRover2019.git"
-
 mkdir ros_copy #Create temp directory
 cd ros_copy #change into temp directory
 
 #Git Process to copy only desired sub(s)/folders
 git init
 git config core.sparseCheckout true
-git remote add origin -f $git_repository
+git remote add origin -f git@github.com:CSUFTitanRover/TitanRover2019.git
 git checkout -b development
 echo "ros/*"> .git/info/sparse-checkout
 git checkout development
+
