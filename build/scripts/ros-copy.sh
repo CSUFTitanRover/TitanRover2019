ba10939c1cac10a332d37d12647959ed86c62955
Can <Jayteare@users.noreply.github.com>
2018-08-05 02:58:42 -0700

    Updated working first draft sparse-copy.

--- build/scripts/ros-copy.sh
+++ build/scripts/ros-copy.sh
@@ -1,3 +1,6 @@
+#Author: Can Dalgir
+#Sparse-Copy Script TitanRover 2019
+
 mkdir ros_copy #Create temp directory
 cd ros_copy #change into temp directory
 
@@ -5,7 +8,22 @@ cd ros_copy #change into temp directory
 git init
 git config core.sparseCheckout true
 git remote add origin -f git@github.com:CSUFTitanRover/TitanRover2019.git
-git checkout -b development
-echo "ros/*"> .git/info/sparse-checkout
-git checkout development
+git checkout -b development #Bare minimum checkout as we only want the branch head pointer
+echo "ros/*"> .git/info/sparse-checkout #Only include the desired sub-directories / files
+git checkout development #Checkout again to populate with desired files
 
+#Validation process for ros, can be more detailed if needed
+if [ -d "ros" ]; then
+
+	cd .. #Go back one level
+	[ -d catkin_ws/src/ ] || mkdir -p catkin_ws/src #validate or create a directory
+	
+	#copy process
+	cd ros_copy #Goes into the temporary repo
+	echo copying ros to catkin_ws/src/
+	cp -R ros ../catkin_ws/src/ #Recursive copying of files to the desired location
+fi
+
+echo copying is finished.
+cd .. #Goes back one level
+rm -r -f ros_copy #removes the temporary repo after finished.
