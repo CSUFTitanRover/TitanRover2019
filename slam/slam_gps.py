77080cb71fd294ec0c4217699c47440e97c4f01b
Timothy Parks <parkstimothyj@csu.fullerton.edu>
2018-11-08 19:36:28 -0800

    Create slam_gps.py

---
+++ slam/slam_gps.py
@@ -0,0 +1,24 @@
+#Timothy Parks mock slam design
+
+gps_arr = [[0 for x in range(1000)] for y in range(1000)] # Empty array unexplored
+
+# 2d array 3281 x 3281 ft
+# 2d array 1000 x 1000 m
+# precision to 0.00001 about 1m
+
+start_lon = -117.882759 # horizontal Fullerton E21 Backdoor
+start_lat = 33.881825  #vertical Fullerton E21 Backdoor
+lat_update = .005
+start_lat += lat_update
+
+for x in range(1000): #(-.005, .005, .00001)
+    for y in range(1000): #(.00001)
+        gps_arr[x][y]=[{"lat": start_lat, "lon": start_lon},-1] #[latitude, longitude, -1 = empty space]
+        start_lon += .00001
+    start_lat -= .00001
+    start_lon = -117.882759 #reset at start of longitude
+
+
+for x in range(500,502): #(-.005, .005, .00001)
+    for y in range(950,999): #(.00001)
+        print gps_arr[x][y][0]['lat'] ,',', gps_arr[x][y][0]['lon']
