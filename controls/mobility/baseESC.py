99900257827ac3b73aea631e12a0fc1091667d3f
skrapmi <parkstimothyj@csu.fullerton.edu>
2018-12-07 17:30:01 -0800

    pushing updates and cleaning house

--- controls/mobility/baseESC.py
+++ controls/mobility/baseESC.py
@@ -81,7 +81,7 @@ else:
 try:    
     # For 433 Mhz Communication
     #/dev/serial/by-id/usb-Silicon_Labs_Rover433_0001-if00-port0
-    rf_uart = serial.Serial('/dev/serial/by-id/usb-Silicon_Labs_Rover433_0001-if00-port0', 19200, timeout=None)
+    rf_uart = serial.Serial('/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0', 19200, timeout=None)
 except:
     print("433 Not Connected")
     sleep(3)
@@ -98,6 +98,7 @@ wheels = DriveEsc(128, "mixed")
 armMix = DriveEsc(129, "notMixed")
 
 # Instantiating The Class Object For LED Lights
+#led = Rover_Status_Lights(60)
 
 '''
 def runPwmPi():
@@ -154,7 +155,7 @@ def getRF(size_of_payload=2):                           #added argument to make 
             n = rf_uart.read(1)                         #the following byte should be the stop byte
             if n == b'f':
                 data = struct.unpack('2b', data)
-                print(data)
+                #print(data)
                 
                 # Publishing to ROS From Base Station
                 msg.header.stamp = rospy.Time.now()
@@ -187,17 +188,19 @@ def putRF(): #arguments to make function more self-contained and function-like
         rf_uart.flush() #waits until all data is written
         sleep(20)
 
+
 def main(data):
     try:
         wheels.driveBoth(data.mobility.ForwardY, data.mobility.TurningX)
-        armMix.driveBoth(data.arm.J2, data.arm.J3)
         #led.update(msg.mode.mode)
+        '''
         if armAttached:
-            #moveJoints([data.arm.J1, data.arm.J4, data.arm.J51, data.arm.J52])
+            moveJoints([data.arm.J1, data.arm.J4, data.arm.J51, data.arm.J52])
             if outVals[-1] != '4':
                 armMix.driveBoth(data.arm.J2, data.arm.J3)                        
             else:
                 armMix.driveBoth(-data.arm.J3, data.arm.J3)    
+        '''        
     except:
         print("Mobility-main-drive error")
 
@@ -223,7 +226,7 @@ if __name__ == '__main__':
         msg = joystick()
         
         threading.Thread(target=getRF).start()
-        threading.Thread(target=putRF).start()
+        #threading.Thread(target=putRF).start()
 
         # Initialize for Subscribe
         #rospy.init_node('listener', anonymous=True)
