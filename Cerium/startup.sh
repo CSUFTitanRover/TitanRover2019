4fdd1b7b7e7795eef8e4a2c6149daeef61ccbf58
David Feinzimer <dfeinzimer@gmail.com>
2019-01-08 23:34:37 -0800

    Script patch

--- Cerium/startup.sh
+++ Cerium/startup.sh
@@ -2,4 +2,4 @@
 
 source /opt/ros/kinetic/setup.bash; roslaunch rosbridge_server rosbridge_websocket.launch &
 cd /home/ubuntu/TitanRover2019/Cerium/; /usr/bin/python -m SimpleHTTPServer &
-/usr/bin/python /home/ubuntu/TitanRover2019/Cerium/appbaseESC.py
+/home/ubuntu/TitanRover2019/Cerium/appbaseESC.py
