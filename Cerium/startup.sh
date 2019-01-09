02b8c1c195247820664a68fd20bbdc3e85088099
David Feinzimer <dfeinzimer@gmail.com>
2019-01-08 22:53:41 -0800

    new script

--- Cerium/startup.sh
+++ Cerium/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-roslaunch rosbridge_server rosbridge_websocket.launch &
-cd ~/TitanRover2019/Cerium; python -m SimpleHTTPServer &
-cd ~/TitanRover2019/Cerium; python appbaseESC.py
+source /opt/ros/kinetic/setup.bash; roslaunch rosbridge_server rosbridge_websocket.launch &
+cd /home/ubuntu/TitanRover2019/Cerium/; /usr/bin/python -m SimpleHTTPServer &
+/usr/bin/python /home/ubuntu/TitanRover2019/Cerium/appbaseESC.py
