ac552aedebb0af486a71508b11b039a29e507734
Georden Grabuskie <ggrabuskie@csu.fullerton.edu>
2019-01-25 00:16:27 -0800

    updated Cerium'

---
+++ nvidia@192.168.1.2/Cerium/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /opt/ros/kinetic/setup.bash; roslaunch rosbridge_server rosbridge_websocket.launch &
+cd /home/ubuntu/TitanRover2019/Cerium/; /usr/bin/python -m SimpleHTTPServer
