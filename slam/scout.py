3623666364199405248a04e6df08fa208750ef19
skrapmi <parkstimothj@csu.fullerton.edu>
2019-02-10 12:48:51 -0800

    changes to scout mode

--- slam/scout.py
+++ slam/scout.py
@@ -4,33 +4,43 @@ import sys
 ######################################################################################
 # System Requirement of one argument for process instructions
 if len (sys.argv) != 2 :
-    print "Usage: Run Command Missing "
+    print("Usage: Run Command Missing ")
     sys.exit (1)
 
 import rospy
 from sensor_msgs.msg import LaserScan
-    
+from finalimu.msg import fimu
+
+mode_info = None
+acceleration = 0
 curr_pos = []
 
+def update_acceleration(data):
+    global acceleration
+    acceleration = data.accel
+
 def store_info(data):
-    global scoutfile
-    print(data.pos)
+    global scoutfile, acceleration
+    scoutfile.write(data.pos)
+    #print(data.pos + ' ' + acceleration + '/n')
+
+def mode_update(data):
+    global mode_info
+    mode_info = data.data
 
 
 def start_scouting():
     print("Scouting has begun")
-    rospy.init_node('listener', anonymous=True)
-    
-        
-    rospy.Subscriber("gnss", gps_position, store_info)
-
+    #rospy.init_node('listener', anonymous=True)
+    #rospy.Subscriber("mode", mobility, mode_update)
+    #rospy.Subscriber("imu", fimu, update_acceleration)
+    #rospy.Subscriber("gnss", gps_position, store_info)
+    #ros.spin()
 
 
 
 def parse_map_file():
     print('Parsing the Scout file')
-
-
 
 
 
