b394ea22abf65fbd353572533d55769e5b92cc30
David Feinzimer <dfeinzimer@gmail.com>
2019-02-09 22:06:18 -0800

    Remove old cerium folder

--- ros/Cerium/scripts/hostHTTP.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import subprocess
-subprocess.run(["cd", "/home/nvidia/TitanRover2019/Cerium"])
-subprocess.run(["sudo", "python", "-m", "SimpleHTTPServer"])
-subprocess.run(["nvidia"])
-
