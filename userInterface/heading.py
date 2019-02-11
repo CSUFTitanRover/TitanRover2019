f4aca2ff4034a4aa2f334024ef646bedd4a05858
David Feinzimer <dfeinzimer@gmail.com>
2019-02-10 19:14:50 -0800

    2.10.19.19.14

--- userInterface/heading.py
+++ userInterface/heading.py
@@ -1,23 +1,29 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from finalimu.msg import fimu
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.graphics import *
+from kivy.properties import ListProperty, ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+import random
 import rospy
-from finalimu.msg import fimu
-
 import random
 
 class MainView(BoxLayout):
     def __init__(self, **kwargs):
         super(MainView, self).__init__(**kwargs)
         self.main_text = Label(text="Initial Value = None")
+        self.arr = Image(source='t.png')
         self.add_widget(self.main_text)
+        self.add_widget(self.arr)
         rospy.init_node('listener', anonymous=True)
         Clock.schedule_interval(self.listener, .5)
         
 
     def update(self, data):
-        self.main_text.text = str(data.yaw.yaw)#"Next Value = " + str(random.randint(0,200))
+        self.main_text.text = str(data.yaw.yaw)
+        self.arr.rotation = data.yaw.yaw
 
     def listener(self, dt):
         rospy.Subscriber("imu", fimu, self.update)
