8b1fab10b1e11df10f52f33c09d730ede584916e
Timothy Parks <parkstimothyj@csu.fullerton.edu>
2019-04-04 19:56:31 -0700

    slam map updates and fixes in sql class

--- build/resources/python-packages/roverdb.py
+++ build/resources/python-packages/roverdb.py
@@ -1,12 +1,12 @@
 import sqlite3
 
 class Database():
-    _dbname = None
+    _dbname = 'rover.sqlite3'
     _cur = None
     _conn = None
 
     def __init__(self):
-        _dbname = "rover"
+        #_dbname = str('rover.sqlite3')
         self.create_rover_tables()
 
     def create_rover_tables(self):
@@ -16,9 +16,10 @@ class Database():
             lat	        REAL NOT NULL,
             lon      	REAL NOT NULL,
             type    	TEXT NOT NULL,
-            acc_data    REAL
+            acc_data    REAL NOT NULL
             )
             ''')
+        
         self._cur.execute('''CREATE TABLE IF NOT EXISTS pictures (
             item_id	    INTEGER PRIMARY KEY AUTOINCREMENT,
             pict_name   TEXT NOT NULL,
@@ -28,20 +29,49 @@ class Database():
             servo_off   REAL NOT NULL
             )
             ''')
-
-    def insert(self, value, table_name):
-	self._cur.execute('''INSERT INTO map (lat, lon, type, acc_data) VALUES(?, 0, 0, 0)''', (value,)) 
+        self.close_db()
 
     # Opens all db files and cursor attachments
     def open_db(self):
-        self._conn = sqlite3.connect(str(self._dbname))
+        self._conn = sqlite3.connect(self._dbname)
         self._cur = self._conn.cursor()
         # Let rows be of dict/tuple type
-        self._conn.row_factory = sqlite3.Row
+        #self._conn.row_factory = sqlite3.Row
         print ("Opened database %s as %r" % (self._dbname, self._conn))
+
+    def close_db(self):
+        self._conn.commit()
+        self._conn.close()
+
+    def insertMap(self, table_name, value1 = 0, value2 = 0, value3 = 0, value4 = 0):
+        self.open_db()
+        self._cur.execute('''INSERT INTO map (lat, lon, type, acc_data) VALUES(?, ?, ?, ?)''', (value1, value2, value3, value4)) 
+        self.close_db()
 
     # returns the size of table
     def getTableSize(self, tablename):
-        return (self._cur.execute("SELECT COUNT(*) FROM " + tablename).fetchall())[0][0]
+        self.open_db()
+        total = (self._cur.execute("SELECT COUNT(*) FROM " + tablename).fetchall())[0][0]
+        self.close_db()
+        return total
 
-
+    def getAccelValue(self, lat, lon):
+        self.open_db()
+        lookup = 'SELECT lat, lon, acc_data FROM map WHERE lat =' + str(lat) + ' AND lon = ' + str(lon)
+        val = self._cur.execute(lookup)
+        found = val.fetchone()[2]
+        self.close_db()
+        if found:
+            return found
+        else:
+            return 0
+    
+if __name__ == '__main__':
+    # Testing the file
+    dbexist = True
+    if not dbexist:
+        db = Database()
+        db.insertMap(map,34.43443, -117.23243,'primary', -3.44)
+        print(db.getAccelValue(34.43443, -117.23243))
+    else:
+        pass
