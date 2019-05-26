0cd7a88e35498a05b640dd574eec52b1028e6fb6
Georden Grabuskie <ggrabuskie@csu.fullerton.edu>
2019-05-25 20:51:51 -0700

    forgot to include the message type

--- ros/publisher_example.py
+++ ros/publisher_example.py
@@ -1,10 +1,13 @@
+from gnss.msg import gps
+
 msg = gps() #message variable
 rospy.init_node('gnss') #create and name current node
 gps_pub = rospy.Publisher('/gnss', gps, queue_size=1) #create the publisher object that will publish message of type gps to the node named '/gnss'
 rate = rospy.Rate(20) #not necessary but changes the rate that you publish data
 
 #do your magic to get the tennis ball gps
-
+msg.roverLat = ''
+msg.roverLon = ''
 msg.tennisLat = "" #latitude as a string
 msg.tennisLon = "" #longitude as a string
 
