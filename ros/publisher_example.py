473f65dfd0ce770ef2fc7a37d21e3e5723a614ba
Georden Grabuskie <ggrabuskie@csu.fullerton.edu>
2019-05-25 20:19:38 -0700

    basic example for publishing to a new node

---
+++ ros/publisher_example.py
@@ -0,0 +1,12 @@
+msg = gps() #message variable
+rospy.init_node('gnss') #create and name current node
+gps_pub = rospy.Publisher('/gnss', gps, queue_size=1) #create the publisher object that will publish message of type gps to the node named '/gnss'
+rate = rospy.Rate(20) #not necessary but changes the rate that you publish data
+
+#do your magic to get the tennis ball gps
+
+msg.tennisLat = "" #latitude as a string
+msg.tennisLon = "" #longitude as a string
+
+gps_pub.publish(msg) #publish msg to the node
+
